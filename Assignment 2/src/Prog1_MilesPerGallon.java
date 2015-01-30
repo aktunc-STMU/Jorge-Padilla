45dd8cf78043057392eba5ef9d5424cab24d7fdc
Kawant's <Kawant's@10.102.47.193>
2015-01-30 11:03:39 -0600

    In class programs for 01/30/15

---
+++ Assignment 2/src/Prog1_MilesPerGallon.java
@@ -0,0 +1,23 @@
+//Jorge A. Padilla MPG calculation java program
+import java.util.Scanner;
+
+public class Prog1_MilesPerGallon 
+{
+	public static void main (String[] args)
+	{
+		int miles;
+		int gallons;
+		double mpg;
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter the miles driven:");
+		miles = input.nextInt();
+		
+		System.out.print("Enter the gallons of fuel used: ");
+		gallons = input.nextInt();
+		
+		mpg = miles/gallons;
+		System.out.print("The miles-per-gallon is " + mpg);
+	}
+}
---
+++ Assignment 2/src/Prog2_ComputeBMI.java
@@ -0,0 +1,18 @@
+//Jorge A. Padilla BMI computing java program
+import javax.swing.JOptionPane;
+
+public class Prog2_ComputeBMI 
+{
+	public static void main (String[] args)
+	{
+		String input1 = JOptionPane.showInputDialog("Enter your weight in pounds.");
+		double weight = Integer.parseInt(input1) * 0.45359237;
+		
+		String input2 = JOptionPane.showInputDialog("Enter your height in inches.");
+		double height = Integer.parseInt(input2) * 0.0254;
+		
+		double bmi = weight/(height * height);
+		
+		JOptionPane.showMessageDialog(null, "Your Body Mass Index is " + bmi);
+	}
+}
